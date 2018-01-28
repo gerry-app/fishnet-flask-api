02bba43d14548347e8d550966c2d5f5c89fc6933
Jason Kao <jasonkao85@gmail.com>
2018-01-28 07:20:08 -0500

    added current events

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect, jsonify, request
 import json
+import re
 
 app = Flask(__name__)
 
@@ -13,6 +14,20 @@ def get_data(name):
         data = json.load(dt)
     return jsonify(data.get(name))
 
+URL_PATTERN = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+
+@app.route('/current-events', methods=['GET', 'POST'])
+def current_events():
+    if request.method == 'POST':
+        url = request.form.get('url', '')
+        if re.search(URL_PATTERN, url):
+            with open('static/current-events.txt', 'a') as news:
+                news.write(url + '\n')
+            return jsonify(response=True)
+        return jsonify(response=False, url=url)
+    with open('static/current-events.txt') as news:
+        data = news.read().split('\n')[:-1]
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
